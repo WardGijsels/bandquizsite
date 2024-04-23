4e802eb7e6daee95326a91ec1d197ca2463d32c7
WardGijsels <r0994450@student.thomasmore.be>
2024-04-23 19:26:38 +0200

    bands added

--- js/bands.js
+++ js/bands.js
@@ -89,6 +89,78 @@ function getAllBands () {
             img: "LornaShore.png",
             nationality: "us",
             mostpopularsong: "To the Hellfire"
+        }, 
+        {
+            name: "Sleep Token",
+            groupsize: 4,
+            genre: "Progressive Metal",
+            debutreleaseyear: 2019,
+            img: "SleepToken.png",
+            nationality: "gb",
+            mostpopularsong: "The Summoning"
+        },
+        {
+            name: "BABYMETAL",
+            groupsize: 3,
+            genre: "Kawaii Metal",
+            debutreleaseyear: 2015,
+            img: "BABYMETAL.png",
+            nationality: "jp",
+            mostpopularsong: "Gimme Chocolat!!"
+        },
+        {
+            name: "Bad Omens",
+            groupsize: 4,
+            genre: "Metalcore",
+            debutreleaseyear: 2016,
+            img: "badomens.png",
+            nationality: "us",
+            mostpopularsong: "Just Pretend"
+        }, 
+        {
+            name: "Within Destruction",
+            groupsize: 3,
+            genre: "Deathcore",
+            debutreleaseyear: 2012,
+            img: "withindestruction.png",
+            nationality: "si",
+            mostpopularsong: "No Way Out"
+        }, 
+        {
+            name: "Brand Of Sacrifice",
+            groupsize: 5,
+            genre: "Deathcore",
+            debutreleaseyear: 2019,
+            img: "brandofsacrifice.png",
+            nationality: "ca",
+            mostpopularsong: "Eclipse"
+        }, 
+        {
+            name: "Stray From The Path",
+            groupsize: 4,
+            genre: "Metalcore",
+            debutreleaseyear: 2008,
+            img: "strayfromthepath.png",
+            nationality: "ca",
+            mostpopularsong: "First World Problem Child"
+        }, 
+        {
+            name: "Electric Callboy",
+            groupsize: 6,
+            genre: "Electronicore",
+            debutreleaseyear: 2012,
+            img: "electriccallboy.png",
+            nationality: "de",
+            mostpopularsong: "We Got The Moves"
+        }, 
+        {
+            name: "Avenged Sevenfold",
+            groupsize: 5,
+            genre: "Progressive Metal",
+            debutreleaseyear: 2002,
+            img: "A7X.png",
+            nationality: "us",
+            mostpopularsong: "Hail To The King"
         }
     ]
 }
