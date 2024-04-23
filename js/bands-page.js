c26126858668e9296c725250522f7851cbb7a2e2
WardGijsels <r0994450@student.thomasmore.be>
2024-04-23 17:38:20 +0200

    displayBandCards function

--- js/bands-page.js
+++ js/bands-page.js
@@ -1,10 +1,34 @@
 const bandselement = document.querySelector("#bandsList");
 const bands = getAllBands();
 
-function displayBandCards () {
-    
+function displayBandCards() {
+    bands.forEach(band => {
+        const div = document.createElement('div');
+        div.classList.add('card', 'text-bg-dark', 'col-4');
+
+        const img = document.createElement('img');
+        img.src = `/img/bands/${band.img}`;
+        img.classList.add('card-img');
+        img.alt = '';
+        img.height = 400;
+        img.id = 'bandImage';
+
+        const overlayDiv = document.createElement('div');
+        overlayDiv.classList.add('card-img-overlay');
+
+        const title = document.createElement('h5');
+        title.classList.add('card-title');
+        title.textContent = band.name;
+        title.id = 'bandName';
+
+        overlayDiv.appendChild(title);
+        div.appendChild(img);
+        div.appendChild(overlayDiv);
+
+        bandselement.appendChild(div);
+    });
 };
 
 addEventListener('DOMContentLoaded', () => {
-
+    displayBandCards();
 });
